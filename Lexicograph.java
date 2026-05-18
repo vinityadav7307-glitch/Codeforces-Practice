3568dfc4fafdaabcd9a57ce0e3b8fc4c2bc6b0fa
Vinit Yadav <vinityadav7307@gmail.com>
2026-05-18 22:17:59 +0530

    Codeforces Question Practice

---
+++ Lexicograph.java
@@ -0,0 +1,46 @@
+// Little Petya loves presents. His mum bought him two strings of the same size for his birthday. The strings consist of uppercase and lowercase Latin letters. Now Petya wants to compare those two strings lexicographically. The letters' case does not matter, that is an uppercase letter is considered equivalent to the corresponding lowercase letter. Help Petya perform the comparison.
+
+// Input
+// Each of the first two lines contains a bought string. The strings' lengths range from 1 to 100 inclusive. It is guaranteed that the strings are of the same length and also consist of uppercase and lowercase Latin letters.
+
+// Output
+// If the first string is less than the second one, print "-1". If the second string is less than the first one, print "1". If the strings are equal, print "0". Note that the letters' case is not taken into consideration when the strings are compared.
+
+// Examples
+// InputCopy
+// aaaa
+// aaaA
+// OutputCopy
+// 0
+// InputCopy
+// abs
+// Abz
+// OutputCopy
+// -1
+// InputCopy
+// abcdefg
+// AbCdEfF
+// OutputCopy
+// 1
+
+import java.util.*;
+public class Lexicograph {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in);
+       String str1 = sc.next().toLowerCase();
+       String str2 = sc.next().toLowerCase();
+       
+       int result = str1.compareTo(str2);
+
+       if(result<0){
+        System.out.println("-1");
+       }
+       else if(result>0){
+        System.out.println("1");
+       }
+       else{
+        System.out.println("0");
+       }
+       sc.close();
+   } 
+}
